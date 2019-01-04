1cba54f3ad672f3adb797b4173f849086a46940e
MZFrontEnd <mea.passiones@gmail.com>
2019-01-04 20:35:03 +0100

    Updates

---
+++ precache-manifest.816e14996b6c303dec89e30b310f085d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "77ecf82aa15c2147b2e25e9da9c5c269",
+    "url": "/static/media/headerImg1.77ecf82a.jpg"
+  },
+  {
+    "revision": "a1b20ea68f320cae939f",
+    "url": "/static/css/main.8e51f732.chunk.css"
+  },
+  {
+    "revision": "b84224f4932fe574bf1f",
+    "url": "/static/js/1.b84224f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3aeb3bda2b74b66f752cd38ae9ae5ef",
+    "url": "/static/media/partnerImg.f3aeb3bd.jpg"
+  },
+  {
+    "revision": "1891225e00d58db83b5b09ef7d3c7e1b",
+    "url": "/static/media/aboutUsImg.1891225e.jpg"
+  },
+  {
+    "revision": "d8d9d14ff203329c9920a89a2c188a24",
+    "url": "/static/media/people.d8d9d14f.jpg"
+  },
+  {
+    "revision": "a1b20ea68f320cae939f",
+    "url": "/static/js/main.a1b20ea6.chunk.js"
+  },
+  {
+    "revision": "1cf34c1d2d7f54d4e9f8725f44755ee4",
+    "url": "/static/media/headerImg.1cf34c1d.jpg"
+  },
+  {
+    "revision": "c6c9a9d888471afdf994f1176f566a9c",
+    "url": "/static/media/conferenceImg.c6c9a9d8.jpg"
+  },
+  {
+    "revision": "400b379dd74a90a60dc25574a5832450",
+    "url": "/static/media/contact.400b379d.jpg"
+  },
+  {
+    "revision": "6dd9d1063d6c3458e7d89e7050c98f4b",
+    "url": "/static/media/conferenceImg2.6dd9d106.jpg"
+  },
+  {
+    "revision": "2fd2a756336b25152fb7170f2d96863d",
+    "url": "/static/media/headerImg1Dark.2fd2a756.jpg"
+  },
+  {
+    "revision": "6c500e5235bf8bad049cf89dd3baa026",
+    "url": "/index.html"
+  }
+];
