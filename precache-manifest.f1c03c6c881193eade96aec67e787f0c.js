b2c413d3643a7c945b13c7d771f1c2238ee17cad
MZFrontEnd <mea.passiones@gmail.com>
2019-01-08 22:02:02 +0100

    Updates

---
+++ precache-manifest.f1c03c6c881193eade96aec67e787f0c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6c9a9d888471afdf994f1176f566a9c",
+    "url": "/static/media/conferenceImg.c6c9a9d8.jpg"
+  },
+  {
+    "revision": "4bf3d24a2520f2f6533f",
+    "url": "/static/css/main.6013fc55.chunk.css"
+  },
+  {
+    "revision": "6c3603f274dd87a8554d",
+    "url": "/static/js/1.6c3603f2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3aeb3bda2b74b66f752cd38ae9ae5ef",
+    "url": "/static/media/partnerImg.f3aeb3bd.jpg"
+  },
+  {
+    "revision": "1891225e00d58db83b5b09ef7d3c7e1b",
+    "url": "/static/media/aboutUsImg.1891225e.jpg"
+  },
+  {
+    "revision": "d8d9d14ff203329c9920a89a2c188a24",
+    "url": "/static/media/people.d8d9d14f.jpg"
+  },
+  {
+    "revision": "77ecf82aa15c2147b2e25e9da9c5c269",
+    "url": "/static/media/headerImg1.77ecf82a.jpg"
+  },
+  {
+    "revision": "1cf34c1d2d7f54d4e9f8725f44755ee4",
+    "url": "/static/media/headerImg.1cf34c1d.jpg"
+  },
+  {
+    "revision": "4bf3d24a2520f2f6533f",
+    "url": "/static/js/main.4bf3d24a.chunk.js"
+  },
+  {
+    "revision": "400b379dd74a90a60dc25574a5832450",
+    "url": "/static/media/contact.400b379d.jpg"
+  },
+  {
+    "revision": "6dd9d1063d6c3458e7d89e7050c98f4b",
+    "url": "/static/media/conferenceImg2.6dd9d106.jpg"
+  },
+  {
+    "revision": "2fd2a756336b25152fb7170f2d96863d",
+    "url": "/static/media/headerImg1Dark.2fd2a756.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/static/media/admin4.a4272d18.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/static/media/admin3.a4272d18.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/static/media/admin.a4272d18.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/static/media/admin2.a4272d18.jpg"
+  },
+  {
+    "revision": "10519e6a2e2a97ee639715dc213e7836",
+    "url": "/index.html"
+  }
+];
